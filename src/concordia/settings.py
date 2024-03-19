7f0ca552850eb800ecc5edefcbfe8045f74a4c92
Matthew Gidden <matthew.gidden@gmail.com>
2024-03-19 08:29:35 +0100

    add a regionmapping to the config

--- src/concordia/settings.py
+++ src/concordia/settings.py
@@ -4,6 +4,7 @@ from typing import Self
 import yaml
 from attrs import define
 from cattrs import structure, transform_error
+from cattrs.errors import ClassValidationError
 
 
 Pathy = str | Path
@@ -24,6 +25,7 @@ class Settings:
 
     base_year: int
     luc_sectors: list[str]
+    regionmappings: dict[str, dict[str, str]]
     country_combinations: dict[str, list[str]]
     variable_template: str
 
@@ -41,7 +43,8 @@ class Settings:
     scenario_path: Path
     # where proxies are stored
     proxy_path: Path
-    # where gridding process files are kept (should contain non-ceds-input and ceds-input folders for proxy generation)
+    # where gridding process files are kept (should contain non-ceds-input and
+    # ceds-input folders for proxy generation)
     gridding_path: Path
     # where postprocessing related files are kept
     postprocess_path: Path
@@ -52,20 +55,29 @@ class Settings:
 
         def expand(path):
             if path[0] == "$":
-                ind = path.index("/")
-                reference = path[1:ind]
-                return expand(config[reference]) / path[ind + 1 :]
+                reference, relative = path[1:].split("/", 1)
+                return expand(config[reference]) / relative
 
             if base_path is not None:
                 return (base_path / path).expanduser()
 
             return Path(path).expanduser()
 
-        return {
+        expanded = {
             key: (expand(val) if key.endswith("_path") else val)
             for key, val in config.items()
             if key != "base_path"
         }
+
+        # Special treatment for path entry in regionmappings
+        regionmappings = config.get("regionmappings")
+        if regionmappings is not None:
+            expanded["regionmappings"] = {
+                key: val | dict(path=expand(val["path"]))
+                for key, val in regionmappings.items()
+            }
+
+        return expanded
 
     @classmethod
     def from_config(cls, config_path: Pathy = "config.yaml", **overwrites) -> Self:
@@ -75,5 +87,5 @@ class Settings:
         # TODO might want to replace with merge for nested dictionaries
         try:
             return structure(cls.resolve_paths(config | overwrites), cls)
-        except Exception as exc:
+        except ClassValidationError as exc:
             raise ValueError(", ".join(transform_error(exc, path="config"))) from None
