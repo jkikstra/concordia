7cb2006c5b7d36db781c4fec527c27fae4fbdfdc
eahoegner <hoegner@iiasa.ac.at>
2025-08-27 17:08:42 +0200

    added shipping proxy creation

--- notebooks/gridding_data/generate_proxies_from_CMIP7_CEDS.py
+++ notebooks/gridding_data/generate_proxies_from_CMIP7_CEDS.py
@@ -81,7 +81,13 @@ sector_mapping = {
     6: "WST"
 }
 
+sector_mapping_shp = {
+    7: "SHP"
+}
+
 # %%
+# ANTHRO proxies
+
 # loop through all CEDS em-anthro from input4MIP files
 for file in ceds_data_location.glob("*.nc"):
 
@@ -137,12 +143,72 @@ for file in ceds_data_location.glob("*.nc"):
     with ProgressBar():
         ds_reordered.to_netcdf(outfile, mode="w", compute=True)
 
+# %%
+# SHIPPING proxies
+
+# loop through all CEDS em-anthro from input4MIP files
+for file in ceds_data_location.glob("*.nc"):
+
+    # extract species information from filename
+    species = file.stem.split("-")[0]
+
+    # import file 
+    ds = xr.open_dataset(
+        file,
+        engine="netcdf4",
+        chunks={"time": 12},
+        lock=lock
+    )
+
+    # drop variables we don't need and rename the one we need
+    ds = ds.drop_vars(["lat_bnds", "lon_bnds", "time_bnds", "sector_bnds"]).rename({f"{species}_em_anthro": "emissions"})
+
+    # retain only SHP sector
+    ds = ds.sel(sector=7)
+
+    # add gas dimension
+    ds = ds.expand_dims(dim={"gas": [f"{species}"]})
+    
+    # rename NMVOC to VOC    
+    if species == "NMVOC":
+        ds = ds.assign_coords(gas=["VOC"])
+        species = "VOC"
+        
+    # rename SO2 to Sulfur
+    if species == "SO2":
+        ds = ds.assign_coords(gas=["Sulfur"])
+        species = "Sulfur"
+    
+    # split time into year and month
+    ds = ds.assign_coords(year=("time", ds["time"].dt.year.data), month=("time", ds["time"].dt.month.data)).groupby(["year", "month"]).mean()
+
+    # select 2023 data and project it onto future years
+    ds = ds.sel(year=2023).expand_dims({"year": years})
+
+    # rename shipping sector and reorder dimensions
+    ds = ds.expand_dims(sector=[ds.sector.item()])
+    ds = ds.assign_coords(sector=("sector", ["SHP" if v == 7 else v for v in ds["sector"].values]))    
+
+
+    ds_reordered = xr.Dataset(
+    {var: (("lat", "lon", "gas", "sector", "year", "month"), ds[var].transpose("lat", "lon", "gas", "sector", "year", "month").values)
+     for var in ds.data_vars},
+    coords={dim: ds[dim] for dim in ["lat", "lon", "gas", "sector", "year", "month"]}
+    ).chunk({"month": 12})
+
+    outfile = new_proxies_location / f"shipping_{species}.nc"
+    if outfile.exists():
+        outfile.unlink()
+
+    with ProgressBar():
+        ds_reordered.to_netcdf(outfile, mode="w", compute=True)
+
 # %% [markdown]
 # ## check one of the new proxy rasters
 
 # %%
 proxy = xr.open_dataset(
-        Path(new_proxies_location, "anthro_BC.nc"),
+        Path(new_proxies_location, "shipping_N2O.nc"),
         engine="netcdf4",
         lock=lock
     )
